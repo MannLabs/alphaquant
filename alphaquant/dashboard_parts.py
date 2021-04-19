7ff4480c142a2663ad4aa1eaad24a17dc279b4cd
EVoytik <jane.voytik@gmail.com>
2021-04-19 15:42:03 +0200

    feat: create a special file with the basic classes of the dashboard will be replaced soon by alphautils package

---
+++ alphaquant/dashboard_parts.py
@@ -0,0 +1,81 @@
+import os
+
+# visualization
+import panel as pn
+
+class HeaderWidget(object):
+    """This class creates a layout for the header of the dashboard with the name of the tool and all links to the MPI website, the MPI Mann Lab page and the GitHub repo.
+
+    Parameters
+    ----------
+    title : str
+        The name of the tool.
+
+    Attributes
+    ----------
+    header_title : pn.pane.Markdown
+        A Panel Markdown pane that returns the title of the tool.
+    mpi_biochem_logo : pn.pane.PNG
+        A Panel PNG pane that embeds a png image file of the MPI Biochemisty logo and makes the image clickable with the link to the official website.
+    mpi_logo : pn.pane.JPG
+        A Panel JPG pane that embeds a jpg image file of the MPI Biochemisty logo and makes the image clickable with the link to the official website.
+    github_logo : pn.pane.PNG
+        A Panel PNG pane that embeds a png image file of the GitHub logo and makes the image clickable with the link to the GitHub repository of the project.
+
+    """
+
+    def __init__(self,
+        title,
+        img_folder_path,
+        github_url
+        ):
+        self.header_title = pn.pane.Markdown(
+            f'# {title}',
+            sizing_mode='stretch_width',
+        )
+        self.biochem_logo_path = os.path.join(
+            img_folder_path,
+            "mpi_logo.png"
+        )
+        self.mpi_logo_path = os.path.join(
+            img_folder_path,
+            "max-planck-gesellschaft.jpg"
+        )
+        self.github_logo_path = os.path.join(
+            img_folder_path,
+            "github.png"
+        )
+
+        self.mpi_biochem_logo = pn.pane.PNG(
+            self.biochem_logo_path,
+            link_url='https://www.biochem.mpg.de/en',
+            width=60,
+            height=60,
+            align='start'
+        )
+        self.mpi_logo = pn.pane.JPG(
+            self.mpi_logo_path,
+            link_url='https://www.biochem.mpg.de/en',
+            height=62,
+            embed=True,
+            width=62,
+            margin=(5, 0, 0, 5),
+            css_classes=['opt']
+        )
+        self.github_logo = pn.pane.PNG(
+            self.github_logo_path,
+            link_url=github_url,
+            height=70,
+            align='end'
+        )
+
+
+    def create(self):
+        return pn.Row(
+            self.mpi_biochem_logo,
+            self.mpi_logo,
+            self.header_title,
+            self.github_logo,
+            height=73,
+            sizing_mode='stretch_width'
+        )
