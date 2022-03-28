e09f2eff974829f3401e4eb215fb3d5cd3e3d62b
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2022-03-28 18:58:43 +0200

    add quicktest workflow

--- tests/download_testfiles.py
+++ tests/download_testfiles.py
@@ -1,7 +1,22 @@
+import sys
 import alphaquant.testfile_handling
 test_folder = "."
-links_yaml = "../alphaquant/configs/download_links_for_testfiles.yaml"
+links_yaml_all_testfiles = "../alphaquant/configs/download_links_for_testfiles_all.yaml"
+links_yaml_quicktest_files = "../alphaquant/configs/download_links_for_testfiles_quicktest.yaml"
+
+def get_links_yaml_file_as_specified(command_line_arguments):
+    if command_line_arguments[1] == 'quicktest':
+        return links_yaml_quicktest_files
+    if command_line_arguments[1] == 'all_tests':
+        return links_yaml_all_testfiles
 
 
-testfieldownloader = alphaquant.testfile_handling.TestFileDownloader(test_folder=test_folder, links_yaml=links_yaml)
-testfieldownloader.download_missing_files()
+if __name__ == '__main__':
+    command_line_arguments = sys.argv
+    try:
+        links_yaml = get_links_yaml_file_as_specified(command_line_arguments)
+    except:
+        raise ValueError("specify if \'quicktest\' or \'all_tests\' on command line")
+
+    testfieldownloader = alphaquant.testfile_handling.TestFileDownloader(test_folder=test_folder, links_yaml=links_yaml)
+    testfieldownloader.download_missing_files()
---
+++ tests/run_quicktests.sh
@@ -0,0 +1,4 @@
+conda activate alphaquant
+python download_testfiles.py quicktest
+jupyter nbconvert --execute --to ./sensitivity_tests/lfq_set.ipynb
+conda deactivate
