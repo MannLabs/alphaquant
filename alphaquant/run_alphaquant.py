52e3db9e3dae295cbd71d6862b68a0a9cb6d16bd
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2023-11-13 10:48:18 +0100

    add organism option for ptm mapping

--- alphaquant/run_alphaquant.py
+++ alphaquant/run_alphaquant.py
@@ -30,7 +30,7 @@ import alphaquant.runner.multi_condition_analysis as aqmulticond
 
 def run_pipeline(*,input_file = None, samplemap_file=None, samplemap_df = None, ml_input_file = None,modification_type = None, input_type_to_use = None,results_dir = "./results", multicond_median_analysis = False,
                  condpair_combinations = None, file_has_alphaquant_format = False,minrep = 2, min_num_ions = 1, minpep = 1, cluster_threshold_pval = 0.2, cluster_threshold_fcfc = 0, fcdiff_cutoff_clustermerge = 0.5, use_ml = True, take_median_ion = True,
-                 outlier_correction = True, normalize = True, use_iontree_if_possible = True, write_out_results_tree = True, get_ion2clust = False, median_offset = False, 
+                 outlier_correction = True, normalize = True, use_iontree_if_possible = True, write_out_results_tree = True, get_ion2clust = False, median_offset = False, organism_for_ptm_mapping = "human",
                  pre_normed_intensity_file = None, dia_fragment_selection = False, use_multiprocessing = False,runtime_plots = False, volcano_fdr =0.05, volcano_fcthresh = 0.5,
                  annotation_file = None, protein_subset_for_normalization_file = None, protnorm_peptides = True):
 
@@ -44,7 +44,7 @@ def run_pipeline(*,input_file = None, samplemap_file=None, samplemap_df = None, 
         samplemap_df = aqutils.load_samplemap(samplemap_file)
 
     if modification_type is not None:
-        input_file = write_ptm_mapped_input(input_file=input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type)
+        input_file = write_ptm_mapped_input(input_file=input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type, organism = organism_for_ptm_mapping)
 
     if "aq_reformat.tsv" not in input_file and not file_has_alphaquant_format:
         input_file = abquantreader.reformat_and_save_input_file(input_file, input_type_to_use = input_type_to_use, use_alphaquant_format=True)
@@ -116,11 +116,11 @@ def run_analysis_multiprocess(condpair_combinations, runconfig, num_cores):
 
 import alphaquant.ptm.ptmsite_mapping as aqptm
 
-def write_ptm_mapped_input(input_file, results_dir, samplemap_df, modification_type):
+def write_ptm_mapped_input(input_file, results_dir, samplemap_df, modification_type, organism = "human"):
     try:
-        aqptm.assign_dataset_inmemory(input_file = input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type)
+        aqptm.assign_dataset_inmemory(input_file = input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type, organism=organism)
     except:
-        aqptm.assign_dataset_chunkwise(input_file = input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type)
+        aqptm.assign_dataset_chunkwise(input_file = input_file, results_dir=results_dir, samplemap_df=samplemap_df, modification_type=modification_type, organism=organism)
     mapped_df = pd.read_csv(f"{results_dir}/ptm_ids.tsv", sep = "\t")
     ptm_mapped_file = aqptm.merge_ptmsite_mappings_write_table(input_file, mapped_df, modification_type)
     return ptm_mapped_file
