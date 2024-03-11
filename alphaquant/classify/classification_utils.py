001fd79b0ab2e2f35247b82b00e646998933d23a
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-03-11 11:29:40 +0100

    add function

--- alphaquant/classify/classification_utils.py
+++ alphaquant/classify/classification_utils.py
@@ -6,3 +6,11 @@ def find_node_parent_at_level(node, level):
         node = node.parent
         if node.level == level:
             return node
+
+def find_node_parent_at_type(node, nodetype):
+    if node.type == nodetype:
+        return node
+    while node.parent is not None:
+        node = node.parent
+        if node.type == nodetype:
+            return node
