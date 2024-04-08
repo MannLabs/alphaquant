9fc681582beb081237785766e5c48d9dd2203679
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-04-08 15:54:10 +0200

    add inter percentile range selection function

--- alphaquant/utils/diffquant_utils.py
+++ alphaquant/utils/diffquant_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def filter_proteoform_df(proteoform_df, min_num_peptides=1, quality_score_cutoff=0, fcdiff_cutoff=0.5, likely_phospho = None,keep_reference_proteoform=True):
 
@@ -13,3 +14,19 @@ def filter_proteoform_df(proteoform_df, min_num_peptides=1, quality_score_cutoff
         return pd.concat([proteoform_df_outliers, df_proteoform_ref_lowcorr])
 
     return proteoform_df_outliers
+
+
+def find_non_outlier_indices_ipr(data, threshold=1.5, percentile_lower = 25, percentile_upper = 75):
+    
+    value_lower, value_upper = np.percentile(data, [percentile_lower, percentile_upper])
+    iqr = value_upper - value_lower
+
+    # Calculate the bounds for non-outliers
+    cut_off = iqr * threshold
+    lowest_tolerated_value = value_lower - cut_off
+    highest_tolerated_value = value_upper + cut_off
+
+    # Identify non-outlier indices
+    non_outlier_indices = np.where((data >= lowest_tolerated_value) & (data <= highest_tolerated_value))[0]
+
+    return non_outlier_indices
