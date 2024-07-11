357144417bc558ff2957b3f7acf6e77d9d6a7af6
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-07-11 13:53:52 +0200

    small refactor and change paths

--- tests/download_testfiles.py
+++ tests/download_testfiles.py
@@ -1,21 +1,20 @@
 import sys
 import alphaquant.benchm.testfile_handling
-test_folder = "."
+test_folder = "../test_data/"
 links_yaml_all_testfiles = "../alphaquant/config/download_links_for_testfiles_all.yaml"
 links_yaml_quicktest_files = "../alphaquant/config/download_links_for_testfiles_quicktest.yaml"
 
-def get_links_yaml_file_as_specified(command_line_arguments):
-    if command_line_arguments[1] == 'quicktest':
-        return links_yaml_quicktest_files
-    if command_line_arguments[1] == 'all_tests':
-        return links_yaml_all_testfiles
 
 
 if __name__ == '__main__':
     command_line_arguments = sys.argv
-    try:
-        links_yaml = get_links_yaml_file_as_specified(command_line_arguments)
-    except:
+    type_of_test = command_line_arguments[1]
+
+    if type_of_test == 'quicktest':
+        links_yaml = links_yaml_quicktest_files
+    elif type_of_test == 'all_tests':
+        links_yaml = links_yaml_all_testfiles
+    else: 
         raise ValueError("specify if \'quicktest\' or \'all_tests\' on command line")
 
     testfieldownloader = alphaquant.benchm.testfile_handling.TestFileDownloader(test_folder=test_folder, links_yaml=links_yaml)
