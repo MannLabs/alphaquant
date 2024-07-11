1208d4312ed9a9580b3e9f21744781308696a39d
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-07-11 13:56:00 +0200

    change to nbmake

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,4 +1,8 @@
 conda activate alphaquant
+pip install pytest
+pip install nbmake==1.5.3
 python download_testfiles.py quicktest
-jupyter nbconvert --execute --to ./sensitivity_tests/lfq_set.ipynb
+echo "Running quicktests"
+pytest --nbmake quicktests/mixed_species.ipynb
+pytest --nbmake quicktests/phospho.ipynb
 conda deactivate
