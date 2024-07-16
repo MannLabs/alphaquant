f1b247653783c2d7b09307509f2838dafdf6632c
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-07-16 16:21:15 +0200

    remove download

--- tests/run_quicktests.sh
+++ tests/run_quicktests.sh
@@ -1,7 +1,6 @@
 conda activate alphaquant
 pip install pytest
 pip install nbmake==1.5.3
-python download_testfiles.py quicktests
 echo "Running quicktests"
 pytest --nbmake quicktests/mixed_species.ipynb
 pytest --nbmake quicktests/phospho.ipynb
