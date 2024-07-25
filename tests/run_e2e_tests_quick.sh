a844a6a97977e3db044995fc394c2b34ab87dfd0
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-07-25 16:24:15 +0200

    change printout

--- tests/run_e2e_tests_quick.sh
+++ tests/run_e2e_tests_quick.sh
@@ -2,7 +2,7 @@
 conda activate alphaquant
 pip install pytest
 pip install nbmake==1.5.3
-echo "Running quicktests"
+echo "Running e2e tests quick"
 pytest --nbmake quicktests/mixed_species.ipynb
 pytest --nbmake quicktests/phospho.ipynb
 conda deactivate
