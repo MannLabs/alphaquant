d278cff71588cafc3048d8370b29a71ebdcadac9
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2024-08-26 18:04:42 +0200

    split select_median_fc_leafs into two functions

--- alphaquant/cluster/cluster_utils.py
+++ alphaquant/cluster/cluster_utils.py
@@ -233,16 +233,19 @@ def select_highid_lowcv_leafs(grouped_leafs):
 def select_median_fc_leafs(grouped_leafs):
     grouped_leafs_medianfc = []
     for leafs in grouped_leafs:
-        leafs_fcsorted = sorted(leafs, key = lambda x : x.fc)
-        if len(leafs_fcsorted) < 4:
-            middle_elements = leafs_fcsorted  # Return the whole list if it has less than 3 elements
-            grouped_leafs_medianfc.append(middle_elements)
-        else:
-            mid_index = len(leafs_fcsorted) // 2
-            middle_elements = leafs_fcsorted[mid_index-1:mid_index+2]
-            grouped_leafs_medianfc.append(middle_elements)
+        grouped_leafs_medianfc.append(select_middle_leafs(leafs))
 
     return grouped_leafs_medianfc
+
+def select_middle_leafs(leaf_group):
+    leafs_fcsorted = sorted(leaf_group, key = lambda x : x.fc)
+    if len(leafs_fcsorted) < 4:
+        middle_elements = leafs_fcsorted  # Return the whole list if it has less than 3 elements
+        return middle_elements
+    else:
+        mid_index = len(leafs_fcsorted) // 2
+        middle_elements = leafs_fcsorted[mid_index-1:mid_index+2]
+        return middle_elements
 
 def map_grouped_leafs_to_diffions(grouped_leafs, ionname2diffion):
     grouped_diffions = []
