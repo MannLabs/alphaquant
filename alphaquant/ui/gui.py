62d3986e55e82b1769541ab88f4f139fffefadc0
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-01-21 17:06:21 +0100

    split up dash modules into run_pipeline and visualize

--- alphaquant/ui/gui.py
+++ alphaquant/ui/gui.py
@@ -7,7 +7,7 @@ import pandas as pd
 # visualization
 import panel as pn
 import bokeh.server.views.ws
-import alphaquant.ui.dashboard_parts as dashboard_parts
+import alphaquant.ui.dashboard_parts_run_pipeline as dashboard_parts
 
 
 def get_css_style(
