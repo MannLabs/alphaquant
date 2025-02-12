9e15aca5210e1f184f3f922ad8ddb95a0e102fe9
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-12 15:43:24 +0100

    Bump version: 0.1.1 → 0.1.2-dev0

--- release/macos/build_installer_macos.sh
+++ release/macos/build_installer_macos.sh
@@ -11,7 +11,7 @@ rm -rf dist_pyinstaller build_pyinstaller
 python -m build
 
 # substitute X.Y.Z-devN with X.Y.Z.devN
-WHL_NAME=$(echo "alphaquant-0.1.1-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
+WHL_NAME=$(echo "alphaquant-0.1.2-dev0-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
 pip install "dist/${WHL_NAME}[stable,gui-stable]"
 
 # Creating the stand-alone pyinstaller folder
--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -11,7 +11,7 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
 python -m build
 
 # substitute X.Y.Z-devN with X.Y.Z.devN
-$WHL_NAME = "alphaquant-0.1.1-py3-none-any.whl" -replace '(\d+\.\d+\.\d+)-dev(\d+)', '$1.dev$2'
+$WHL_NAME = "alphaquant-0.1.2-dev0-py3-none-any.whl" -replace '(\d+\.\d+\.\d+)-dev(\d+)', '$1.dev$2'
 pip install "dist/$WHL_NAME[stable,gui-stable]"
 
 # Creating the stand-alone pyinstaller folder
