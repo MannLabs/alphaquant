707b230ee469d5c1dde2da2b99e8a2ae1dca8763
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-12 16:01:58 +0100

    change dependencies

--- misc/loose_pip_install.sh
+++ misc/loose_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n alphaquant python=3.9 -y
 conda activate alphaquant
-pip install -e '../.[development]'
+pip install -e '../.[development, gui]'
 alphaquant
 conda deactivate
--- misc/stable_pip_install.sh
+++ misc/stable_pip_install.sh
@@ -1,5 +1,5 @@
 conda create -n alphaquant python=3.9 -y
 conda activate alphaquant
-pip install -e '../.[stable,development-stable]'
+pip install -e '../.[stable,gui-stable, development]'
 alphaquant
 conda deactivate
