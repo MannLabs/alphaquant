4875f5657a8d5bab7aca9f55219eb3ac1c1256fb
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-12 15:16:38 +0100

    add new release and publish workflows

---
+++ release/macos/build_installer_macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -u
+
+# Build the installer for MacOS.
+# This script must be run from the root of the repository.
+
+rm -rf dist build *.egg-info
+rm -rf dist_pyinstaller build_pyinstaller
+
+# Creating the wheel
+python -m build
+
+# substitute X.Y.Z-devN with X.Y.Z.devN
+WHL_NAME=$(echo "alphaquant-0.1.1-py3-none-any.whl" | sed 's/\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\)-dev\([0-9][0-9]*\)/\1.dev\2/g')
+pip install "dist/${WHL_NAME}[stable,gui-stable]"
+
+# Creating the stand-alone pyinstaller folder
+pip install pyinstaller
+pyinstaller release/pyinstaller/alphaquant.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
+
---
+++ release/windows/build_installer_windows.ps1
@@ -0,0 +1,19 @@
+# Build the installer for Windows.
+# This script must be run from the root of the repository.
+
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./build
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./*.egg-info
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./build_pyinstaller
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
+
+# Creating the wheel
+python -m build
+
+# substitute X.Y.Z-devN with X.Y.Z.devN
+$WHL_NAME = "alphaquant-0.1.1-py3-none-any.whl" -replace '(\d+\.\d+\.\d+)-dev(\d+)', '$1.dev$2'
+pip install "dist/$WHL_NAME[stable,gui-stable]"
+
+# Creating the stand-alone pyinstaller folder
+pip install pyinstaller
+pyinstaller release/pyinstaller/alphaquant.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
