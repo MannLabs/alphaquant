436872f24191349669608f8d0b74c0ecc92e649b
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-12 15:00:22 +0100

    remove old release workflow

--- release/pypi/install_test_pypi_wheel.sh
+++
@@ -1,5 +0,0 @@
-conda create -n alphaquant_pip_test python=3.9 -y
-conda activate alphaquant_pip_test
-pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "alphaquant[stable]"
-alphaquant
-conda deactivate
