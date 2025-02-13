216eeb126735b3c4269644b733215a521471aed7
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-13 09:54:04 +0100

    add condition pair separator constant

--- alphaquant/config/variables.py
+++ alphaquant/config/variables.py
@@ -4,6 +4,7 @@ QUANT_ID = "quant_id"
 MIN_PVAL = 1e-16
 PROGRESS_FOLDER = "progress"
 PREFER_PRECURSORS_FOR_CLUSTERING = True
+CONDITION_PAIR_SEPARATOR = "_VS_"
 
 def determine_variables(input_file, input_type):
     _determine_quant_id(input_file)
@@ -13,7 +14,7 @@ def determine_variables(input_file, input_type):
 def _determine_quant_id(input_file):
     global QUANT_ID
     if "aq_reformat.tsv" in input_file:
-        input_df = pd.read_csv(input_file, sep="\t", nrows=3)        
+        input_df = pd.read_csv(input_file, sep="\t", nrows=3)
         if "quant_id" in input_df.columns:
             QUANT_ID = "quant_id"
         elif "ion" in input_df.columns:
