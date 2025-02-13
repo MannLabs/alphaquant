2946228c2907a7c5e20d72f0a99912390d8aa318
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-13 13:30:32 +0100

    remove comment

--- release/linux/build_package_linux.sh
+++ release/linux/build_package_linux.sh
@@ -9,10 +9,6 @@ PACKAGE_NAME=alphaquant
 # BUILD_NAME is taken from environment variables, e.g. 'alphaquant-1.2.3-linux-x64'
 rm -rf ${BUILD_NAME}.deb
 
-# If needed, include additional source such as e.g.:
-# cp ../../alphaquant/data/*.fasta dist/alphaquant/data
-# WARNING: this probably does not work!!!!
-
 # Wrapping the pyinstaller folder in a .deb package
 mkdir -p dist_pyinstaller/${BUILD_NAME}/usr/local/bin
 mv dist_pyinstaller/${PACKAGE_NAME} dist_pyinstaller/${BUILD_NAME}/usr/local/bin/${PACKAGE_NAME}
