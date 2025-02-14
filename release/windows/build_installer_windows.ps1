3ea5d8ffb54df86c4b05ad78f89ab8ef19d27eab
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-14 13:33:12 +0100

    include alphamap exception

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -9,14 +9,17 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
 $WHL_NAME = (Get-ChildItem -Path "dist" -Filter "*.whl").Name
 
 ## install alphamap
-#git clone alphamap@2345rf
-#cd alphamap
-#pip install .
-#remove alphamap from alphaquant requirements.txt
-#
-## re-build wheel
-#rm -rf dist ./*.egg-info
-#python -m build
+git clone alphamap@c65381a11a0f25fe04822846863520afc966b967
+cd alphamap
+pip install .
+# Remove alphamap from requirements files
+(Get-Content "requirements/requirements.txt") | Where-Object { $_ -notmatch "^alphamap==" } | Set-Content "requirements/requirements.txt"
+(Get-Content "requirements/requirements_loose.txt") | Where-Object { $_ -notmatch "^alphamap$" } | Set-Content "requirements/requirements_loose.txt"
+
+# re-build wheel
+cd ..
+rm -rf dist ./*.egg-info
+python -m build
 
 
 pip install "dist/$WHL_NAME[stable,gui-stable]"
