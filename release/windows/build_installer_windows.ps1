21c7eca3cc2042a985628a3867e94d847da7352e
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-14 11:34:18 +0100

    pseudo code for alphamap install on windows

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -7,6 +7,18 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
 
 # substitute X.Y.Z-devN with X.Y.Z.devN
 $WHL_NAME = (Get-ChildItem -Path "dist" -Filter "*.whl").Name
+
+## install alphamap
+#git clone alphamap@2345rf
+#cd alphamap
+#pip install .
+#remove alphamap from alphaquant requirements.txt
+#
+## re-build wheel
+#rm -rf dist ./*.egg-info
+#python -m build
+
+
 pip install "dist/$WHL_NAME[stable,gui-stable]"
 
 # Creating the stand-alone pyinstaller folder
