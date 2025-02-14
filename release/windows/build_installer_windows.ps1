fab8bf7955278634e4f761a0b99f013e79bc8c27
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-14 13:44:39 +0100

    fix directory change

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -12,12 +12,13 @@ $WHL_NAME = (Get-ChildItem -Path "dist" -Filter "*.whl").Name
 git clone alphamap@c65381a11a0f25fe04822846863520afc966b967
 cd alphamap
 pip install .
+cd ..
 # Remove alphamap from requirements files
 (Get-Content "requirements/requirements.txt") | Where-Object { $_ -notmatch "^alphamap==" } | Set-Content "requirements/requirements.txt"
 (Get-Content "requirements/requirements_loose.txt") | Where-Object { $_ -notmatch "^alphamap$" } | Set-Content "requirements/requirements_loose.txt"
 
 # re-build wheel
-cd ..
+
 rm -rf dist ./*.egg-info
 python -m build
 
