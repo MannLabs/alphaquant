dfb6719a73b105972d19c22d2bf5673eb915765c
ammarcsj <70114795+ammarcsj@users.noreply.github.com>
2025-02-14 15:32:13 -0800

    set windows installer back to how it was

--- release/windows/build_installer_windows.ps1
+++ release/windows/build_installer_windows.ps1
@@ -7,24 +7,8 @@ Remove-Item -Recurse -Force -ErrorAction SilentlyContinue ./dist_pyinstaller
 
 # substitute X.Y.Z-devN with X.Y.Z.devN
 $WHL_NAME = (Get-ChildItem -Path "dist" -Filter "*.whl").Name
+pip install "dist/$WHL_NAME[stable,gui-stable]"
 
-## install alphamap
-git clone https://github.com/MannLabs/alphamap.git@c65381a11a0f25fe04822846863520afc966b967
-cd alphamap
-pip install .
-# cd ..
-# # Remove alphamap from requirements files
-# (Get-Content "requirements/requirements.txt") | Where-Object { $_ -notmatch "^alphamap==" } | Set-Content "requirements/requirements.txt"
-# (Get-Content "requirements/requirements_loose.txt") | Where-Object { $_ -notmatch "^alphamap$" } | Set-Content "requirements/requirements_loose.txt"
-
-# # re-build wheel
-
-# rm -rf dist ./*.egg-info
-# python -m build
-
-
-# pip install "dist/$WHL_NAME[stable,gui-stable]"
-
-# # Creating the stand-alone pyinstaller folder
-# pip install pyinstaller
-# pyinstaller release/pyinstaller/alphaquant.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
+# Creating the stand-alone pyinstaller folder
+pip install pyinstaller
+pyinstaller release/pyinstaller/alphaquant.spec --distpath dist_pyinstaller --workpath build_pyinstaller -y
