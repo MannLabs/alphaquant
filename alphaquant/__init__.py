6bc564e26254c4d7aa47a0a76e1bc34a39bd0f13
null <70114795+ammarcsj@users.noreply.github.com>
2025-11-14 14:46:04 +0000

    Bump version: 0.2.1-dev0 → 0.3.0-dev0

--- alphaquant/__init__.py
+++ alphaquant/__init__.py
@@ -2,7 +2,7 @@
 
 
 __project__ = "alphaquant"
-__version__ = "0.2.1-dev0"
+__version__ = "0.3.0-dev0"
 __license__ = "Apache"
 __description__ = "An open-source Python package of the AlphaPept ecosystem"
 __author__ = "Mann Labs"
